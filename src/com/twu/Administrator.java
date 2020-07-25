823c8996bc07ce9816574d8ec48272856729b6b2
liaowenqiang828 <18840965351@163.com>
2020-07-25 12:01:40 +0800

    [Wenqiang Liao] feat: add a calss called Administrator which extends User

---
+++ src/com/twu/Administrator.java
@@ -0,0 +1,15 @@
+package com.twu;
+
+import java.util.ArrayList;
+
+public class Administrator extends User {
+    @Override
+    public ArrayList<String> checkHotSearch() {
+        return super.checkHotSearch();
+    }
+
+    @Override
+    public ArrayList<String> addHotSearch() {
+        return super.addHotSearch();
+    }
+}
