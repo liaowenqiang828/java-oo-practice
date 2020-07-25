eb22f5c00da9da3b632758191b321b370dd77b9f
liaowenqiang828 <18840965351@163.com>
2020-07-25 12:06:41 +0800

    [Wenqiang Liao] feat: add a calss called HotSearchItem which has three propoties: order, description, hotDegree

---
+++ src/com/twu/HotSearchItem.java
@@ -0,0 +1,13 @@
+package com.twu;
+
+public class HotSearchItem {
+    private int order;
+    private String description;
+    private int hotDegree;
+
+    public HotSearchItem(int order, String description, int hotDegree) {
+        this.order = order;
+        this.description = description;
+        this.hotDegree = hotDegree;
+    }
+}
