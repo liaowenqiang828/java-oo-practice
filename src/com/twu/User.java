af76147539df565d7f536793dabc8227a75e5d19
liaowenqiang828 <18840965351@163.com>
2020-07-25 11:59:06 +0800

    [Wenqiang Liao] feat: User class implements HotSearch interface

--- src/com/twu/User.java
+++ src/com/twu/User.java
@@ -1,4 +1,15 @@
 package com.twu;
 
-public class User {
+import java.util.ArrayList;
+
+public class User implements HotSearch{
+    @Override
+    public ArrayList<String> checkHotSearch() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<String> addHotSearch() {
+        return null;
+    }
 }
