c58f9b457ff9682eb15c1074fef6ad6fc1f3bcaa
liaowenqiang828 <18840965351@163.com>
2020-07-26 15:30:24 +0800

    [Wenqiang Liao] feat: add voteHotSearch and purchaseHotSearch method in NormalUser class

--- src/com/twu/Main.java
+++ src/com/twu/Main.java
@@ -1,17 +1,59 @@
 package com.twu;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-//        ArrayList<HotSearchItem> hotSearchList = new ArrayList<>();
-//
-//        hotSearchList.add(new HotSearchItem(1, "乘风破浪的姐姐 假唱", 2692416));
-//        hotSearchList.add(new HotSearchItem(2, "吴昕拉票时的表情", 1865301));
-//        hotSearchList.add(new HotSearchItem(3, "明日之子 倍儿牛", 1823017));
-//        hotSearchList.add(new HotSearchItem(4, "失踪遇害女子丈夫被抓现场", 1808994));
-//        hotSearchList.add(new HotSearchItem(5, "万茜复工", 1540517));
+    public static void main(String[] args) throws SQLException {
+        connector connector = new connector();
+
+        ArrayList<HotSearchItem> hotSearchList = connector.getSourceDataArrayList();
+        hotSearchList.stream().forEach(hotSearchItem -> System.out.println(hotSearchItem.toString()));
+
+        System.out.println("请按照格式输入用户角色代码：");
+        System.out.println("普通用户请输入数字：0， 管理员用户请输入数字：1， 按Enter键结束。");
+        Scanner scanner = new Scanner(System.in);
+
+        if (scanner.next() != "0" || scanner.next() != "1") {
+            System.out.println("命令输入格式错误，请重新正确输入： ");
+        }
+
+        if (scanner.next() == "0") {
+            System.out.println("进入普通用户操作：");
+            NormalUser normalUser = new NormalUser(hotSearchList);
+
+            System.out.println("请输入你要进行的操作的代码，按Enter键结束");
+            System.out.println("查看热搜排行榜，请输入： check");
+            System.out.println("给热搜事件投票，请输入： vote");
+            System.out.println("购买热搜，请输入： purchase");
+            System.out.println("添加热搜，请输入： add");
+
+            Scanner scanner1 = new Scanner(System.in);
+            String commond1 = scanner1.next();
+
+            switch (commond1) {
+                case "check":
+                    normalUser.checkHotSearch();
+                    break;
+                case "vote":
+                    normalUser.voteHotSearch();
+                    break;
+                case "purchase":
+                    normalUser.purchaseHotSearch();
+                    break;
+                case "add":
+                    normalUser.addHotSearch();
+                    break;
+            }
+
+
+
+
+
+
+        }
 
     }
 }
--- src/com/twu/NormalUser.java
+++ src/com/twu/NormalUser.java
@@ -1,8 +1,14 @@
 package com.twu;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class NormalUser extends User {
+    ArrayList<HotSearchItem> hotSearchList;
+    public NormalUser(ArrayList<HotSearchItem> hotSearchList) {
+        this.hotSearchList = hotSearchList;
+    }
+
     @Override
     public ArrayList<String> checkHotSearch() {
         return super.checkHotSearch();
@@ -11,5 +17,29 @@ public class NormalUser extends User {
     @Override
     public ArrayList<String> addHotSearch() {
         return super.addHotSearch();
+    }
+
+    public void voteHotSearch() {
+        System.out.println("请输入要进行投票热搜事件名称:");
+        Scanner scanner = new Scanner(System.in);
+        String hotSearchName = scanner.next();
+
+        System.out.println("请输入你带投票数目：");
+        Scanner scanner1 = new Scanner(System.in);
+        int hotDegree = Integer.parseInt(scanner1.next());
+
+
+    }
+
+    public void purchaseHotSearch() {
+        System.out.println("请输入购买的热搜事件名称:");
+        Scanner scanner = new Scanner(System.in);
+        String hotSearchName = scanner.next();
+
+        System.out.println("请输入需要购买的热度值：");
+        Scanner scanner1 = new Scanner(System.in);
+        int hotDegree = Integer.parseInt(scanner1.next());
+
+
     }
 }
