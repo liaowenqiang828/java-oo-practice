dbf76fe938daa58c2c3f18140ce88e7ea91bf3db
liaowenqiang828 <18840965351@163.com>
2020-07-27 10:12:33 +0800

    [Wenqiang Liao] feat: add a method named addSuperHotSearch in Administrator class

--- src/com/twu/Administrator.java
+++ src/com/twu/Administrator.java
@@ -1,13 +1,29 @@
 package com.twu;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Administrator extends User {
     public Administrator(ArrayList<HotSearchItem> hotSearchList) {
         super(hotSearchList);
     }
 
-    public void addSperHotSearch() {
+    public void addSuperHotSearch() throws SQLException {
+        System.out.println("请输入要添加的超级热搜事件名称:");
+        Scanner scanner = new Scanner(System.in);
+        String description = scanner.next();
 
+        System.out.println("请输入添加超级热搜的热度值：");
+        Scanner scanner1 = new Scanner(System.in);
+        int hotDegree = Integer.parseInt(scanner1.next());
+
+        Connector connector = new Connector();
+        String sql;
+
+        String sqlFormat = "insert into hot_search values (null, \'%s\', %d, true)";
+        sql = String.format(sqlFormat, description, hotDegree);
+
+        connector.updateDataAfterVoteOrPurchase(sql);
     }
 }
--- src/com/twu/Main.java
+++ src/com/twu/Main.java
@@ -17,10 +17,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String code = scanner.next();
 
-        if (!code.equals("0") || !code.equals("1")) {
-            System.out.println("命令输入格式错误，请重新正确输入： ");
-        }
-
         if (code.equals("0")) {
             System.out.println("进入普通用户操作：");
             NormalUser normalUser = new NormalUser(10, hotSearchList);
@@ -48,14 +44,14 @@ public class Main {
                     normalUser.addHotSearch();
                     break;
             }
-        } else {
+        } else if (code.equals("1")){
             System.out.println("进入管理员用户操作：");
-            Administrator administrator = new Administrator(10, hotSearchList);
+            Administrator administrator = new Administrator(hotSearchList);
 
             System.out.println("请输入你要进行的操作的代码，按Enter键结束");
             System.out.println("查看热搜排行榜，请输入： check");
             System.out.println("添加热搜，请输入： add");
-            System.out.println("添加超级热搜，请输入： add super");
+            System.out.println("添加超级热搜，请输入： add_super");
 
             Scanner scanner1 = new Scanner(System.in);
             String commond1 = scanner1.next();
@@ -67,12 +63,12 @@ public class Main {
                 case "add":
                     administrator.addHotSearch();
                     break;
-                case "add super":
-                    administrator.addSperHotSearch();
+                case "add_super":
+                    administrator.addSuperHotSearch();
                     break;
             }
-
-
+        } else {
+            System.out.println("命令输入格式错误，请重新正确输入： ");
         }
     }
 }
